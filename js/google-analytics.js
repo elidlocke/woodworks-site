1f8c419b0dbe10e022fd61c89ddf8ad4afa9ea94
Elizabeth Locke <elidlocke@gmail.com>
2023-01-18 13:42:31 -0500

    google analytics 4

--- js/google-analytics.js
+++ js/google-analytics.js
@@ -1,10 +1,9 @@
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-37331896-1"></script>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-MRQ6P8EGCK"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'UA-37331896-1');
+  gtag('config', 'G-MRQ6P8EGCK');
 </script>
-
